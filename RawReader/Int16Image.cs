2c2a2cb065defdfb86968d0980e378d61a9988ae
m <mleipper@hotmail.com>
2019-07-30 00:19:36 +0100

    some changes made.

---
+++ RawReader/Int16Image.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RawReader
+{
+    public class Int16Image
+    {
+        private UInt16[] _fileArr;
+        private int _width;
+        private int _height;
+        private int _actualFileSize;
+
+        public Int16Image()
+        {
+
+        }
+
+        public Int16Image(string fileName, int width, int height) {
+            _height = height;
+            _width = width;
+            LoadUint16File(fileName, _width, _height);
+
+        }
+
+        public void LoadUint16File(string fileName, int width, int height) {
+            byte[] fileBytes = File.ReadAllBytes(fileName);
+            int expectedFileSize = height * width;
+            _actualFileSize = fileBytes.Count() / sizeof(UInt16); // Could perform an assert here to see if expected meets actual...
+            _fileArr = new UInt16[_actualFileSize];
+
+            for (var index = 0; index < _actualFileSize; index++)
+            {
+                _fileArr[index] = BitConverter.ToUInt16(fileBytes, index * sizeof(Int16));
+                //Console.WriteLine(_fileArr[index]);
+            }
+
+        }
+
+        public UInt16[] Getbytes()
+        {
+            return _fileArr;
+        }
+
+        public UInt16[] Compare(Int16Image int16Array)
+        {
+            var secondImage = int16Array.Getbytes();
+            UInt16[] newImage = new UInt16[_actualFileSize];
+            for (var index = 0; index < _actualFileSize; index++)
+            {
+                newImage[index] = (UInt16)(_fileArr[index] - secondImage[index]);
+                Console.WriteLine(newImage[index]);
+            }
+            return newImage;
+        }
+
+
+    }
+}
--- RawReader/Program.cs
+++ RawReader/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace RawReader
 {
@@ -7,6 +9,27 @@ namespace RawReader
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Int16Image image1 = new Int16Image("sino800_540x1200.RAW", 1200, 500);
+            
+            
+
         }
+
+        static UInt16[] GetInt16File(string fileName, int width, int height)
+        {
+            byte[] fileBytes = File.ReadAllBytes(fileName);
+            int expectedFileSize = height * width;
+            int actualFileSize = fileBytes.Count() / sizeof(UInt16); // Could perform an assert here to see if expected meets actual...
+            UInt16[] fileArr = new UInt16[actualFileSize];
+
+            for (var index = 0; index < actualFileSize; index++)
+            {
+                fileArr[index] = BitConverter.ToUInt16(fileBytes, index * sizeof(Int16));
+                Console.WriteLine(fileArr[index]);
+            }
+
+            return fileArr;
+        }
+        
     }
 }
