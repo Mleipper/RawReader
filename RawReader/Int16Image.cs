2774537fcf396b351c240d48c8f26dec4c65307d
m <mleipper@hotmail.com>
2019-08-01 17:14:32 +0100

    got async working

--- RawReader/Int16Image.cs
+++ RawReader/Int16Image.cs
@@ -11,23 +11,14 @@ namespace RawReader
     public class Int16Image
     {
         public const int MAX_WIDTH_HEIGHT = 10000;
-        private UInt16[] _fileArr;
+        private UInt16[][] _fileArr;
         private int _width;
         private int _height;
         private int _actualFileSize;
         private string _fileName;
+
         public Int16Image()
         {
-
-        }
-        //CONSTRUCTOR USED IN THE CREATION OF A THIRD IMAGE 
-        public Int16Image(string fileName, int width,int height,UInt16[] image,int actualFileSize)
-        {
-            _height = height;
-            _width = width;
-            _fileName = fileName;
-            _actualFileSize = actualFileSize;
-            _fileArr = image;
 
         }
 
@@ -35,6 +26,7 @@ namespace RawReader
             _height = height;
             _width = width;
             _fileName = fileName;
+            InitialiseArr(_width, _height);
         }
         /// <summary>
         /// Produces local Uint16 to represent the current file. 
@@ -46,7 +38,17 @@ namespace RawReader
             _height = height;
             _width = width;
             _fileName = fileName;
+            InitialiseArr(_width, _height);
             LoadUint16File();
+        }                
+
+        private void InitialiseArr(int width, int height)
+        {
+            _fileArr = new UInt16[height][];
+            for(int h = 0; h < height; h++)
+            {
+                _fileArr[h] = new UInt16[width];
+            }
         }
 
         public void LoadUint16File()
@@ -54,71 +56,97 @@ namespace RawReader
             byte[] fileBytes = File.ReadAllBytes(_fileName);
             int expectedFileSize = _height * _width;
             _actualFileSize = fileBytes.Count() / sizeof(UInt16); // Could perform an assert here to see if expected meets actual...
-            _fileArr = new UInt16[_actualFileSize];
 
-            for (var index = 0; index < _actualFileSize; index++)
+            for(int h = 0; h < _height; h++)
             {
-                _fileArr[index] = BitConverter.ToUInt16(fileBytes, index * sizeof(Int16));
-                //Console.WriteLine(_fileArr[index]);
-            }
-        }
-
-        private UInt16[][] _fileArr2d;
-
-        public void InitialiseArr(int width, int height)
-        {
-            _fileArr2d = new UInt16[height][];
-            for(int h = 0; h < height; h++)
-            {
-                _fileArr2d[h] = new UInt16[width];
-            }
-
-        }
-
-        public void LoadUint16File2d(string fileName, int width, int height)
-        {
-            byte[] fileBytes = File.ReadAllBytes(fileName);
-            int expectedFileSize = height * width;
-            _actualFileSize = fileBytes.Count() / sizeof(UInt16); // Could perform an assert here to see if expected meets actual...
-            InitialiseArr(width, height);
-
-            for(int h = 0; h < height; h++)
-            {
-                for(int w = 0; w < width; w++)
+                for(int w = 0; w < _width; w++)
                 {
-                    var fileIndex = (h * width) + w;
-                    _fileArr2d[h][w] = BitConverter.ToUInt16(fileBytes, fileIndex * sizeof(Int16));
+                    var fileIndex = (h * _width) + w;
+                    _fileArr[h][w] = BitConverter.ToUInt16(fileBytes, fileIndex * sizeof(Int16));
                 }
             }
         }
-
-        public Int16Image CompareImages2d(Int16Image int16Array)
+        /// <summary>
+        /// non async method of compare images 
+        /// </summary>
+        /// <param name="secImg"></param>
+        /// <returns></returns>
+        public Int16Image CompareImages(Int16Image secImg)
         {
-            var secondImage = int16Array.Getbytes2d();
-            var newImage = new Int16Image("comparison of" + _fileName + " " + int16Array.GetFileName(), _width, _height);
-            newImage.InitialiseArr(_width, _height);
-            UInt16[][] newImageArr = newImage.Getbytes2d();
-            for (var height = 0; height < _height; height++)
+            if (!IsSameDimension(secImg))
             {
-                for (int width = 0; width < _width; width++)
-                    newImageArr[height][width] = (UInt16)(_fileArr2d[height][width] - secondImage[height][width]);
-
+                throw new ArgumentOutOfRangeException("Images must be the same width and height!");
             }
-            newImage.SetArr2d(newImageArr);
+            var newImage = new Int16Image("comparison of" + _fileName + " " + secImg.GetFileName(), _width, _height);
+
+            List<Task> imgTasks = new List<Task>();
+
+            for (var h = 0; h < _height; h++)
+            {
+                int row = h;
+                newImage.CompareRow(row, this, secImg);
+            }
+
+            return newImage;
+        }
+        /// <summary>
+        /// async method to compare 
+        /// </summary>
+        /// <param name="secImg"></param>
+        /// <returns></returns>
+        public async Task<Int16Image> CompareImagesAsync(Int16Image secImg)
+        {
+            if (!IsSameDimension(secImg))
+            {
+                throw new ArgumentOutOfRangeException("Images must be the same width and height!");
+            }
+            var newImage = new Int16Image("comparison of" + _fileName + " " + secImg.GetFileName(), _width, _height);
+
+            List<Task> imgTasks = new List<Task>();
+
+            for (var h = 0; h < _height; h++)
+            {
+                int row = h;                
+                imgTasks.Add(Task.Run(() => newImage.CompareRow(row, this, secImg)));
+            }
+            
+            while (imgTasks.Any())
+            {
+                Task finished = await Task.WhenAny(imgTasks);
+                Console.WriteLine("Row complete");
+                imgTasks.Remove(finished);
+            }
+
             return newImage;
         }
 
-        public UInt16[][] Getbytes2d()
+
+        private void CompareRow(int row, Int16Image firstImg, Int16Image secImg)
         {
-            return _fileArr2d;       
+            var firstImgRow = firstImg.GetRow(row);
+            var secImgRow = secImg.GetRow(row);
+            for (int w = 0; w < _width; w++)
+            {
+                _fileArr[row][w] = (UInt16)(firstImgRow[w] - secImgRow[w]);
+            }
         }
 
-        public void SetArr2d(UInt16[][] SetArr2d)
+        public bool IsSameDimension(Int16Image otherImg)
         {
-            _fileArr2d = SetArr2d;
+            return _width == otherImg._width && _height == otherImg._height;
         }
 
-        public UInt16[] Getbytes()
+        public UInt16[] GetRow(int row)
+        {
+            return _fileArr[row];
+        }
+
+        public void SetArr(UInt16[][] SetArr)
+        {
+            _fileArr = SetArr;
+        }
+
+        public UInt16[][] Getbytes()
         {
             return _fileArr;
         }
@@ -134,39 +162,15 @@ namespace RawReader
             return _fileName;
         }
 
-        //can be made async thruouh use of a 2 d array
-        public Int16Image CompareImages(Int16Image int16Array)
-        {
-            var secondImage = int16Array.Getbytes();
-            UInt16[] newImage = new UInt16[_actualFileSize];
-            for (var index = 0; index < _actualFileSize; index++)
-            {
-                newImage[index] = (UInt16)(_fileArr[index] - secondImage[index]);
-  
-            }
-            var newInt16 = new Int16Image("comparison of" + _fileName + " " + int16Array.GetFileName(), _width, _height, newImage, _actualFileSize);
-            return newInt16;
-        }
-
-        public void Output2dToFile()
+        public void OutputToFile()
         {
             byte[] result = new byte[GetFileSize()];
             for(int h = 0; h < _height; h++)
             {
-                Buffer.BlockCopy(_fileArr2d[h], 0, result, (h * _width), _width * sizeof(UInt16));
+                Buffer.BlockCopy(_fileArr[h], 0, result, (h * _width), _width * sizeof(UInt16));
             }
             
             File.WriteAllBytesAsync(_fileName, result);
-        }
-    
-
-        public  void OutPutTofile()
-        {
-            byte[] result = new byte[_fileArr.Length * sizeof(UInt16)];
-            Buffer.BlockCopy(_fileArr, 0, result, 0, result.Length);
-            File.WriteAllBytesAsync(_fileName, result);
-        }
-
-
+        } 
     }
 }
--- RawReader/Program.cs
+++ RawReader/Program.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace RawReader
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             //Int16Image image1 = new Int16Image("sino800_540x1200.RAW", 1200, 500);
             //Int16Image image2 = new Int16Image("sino801_540x1200.RAW", 1200, 500);
             //var image3 = image1.CompareImages(image2);
             //image3.OutPutTofile();
-            var image1= new Int16Image("sino800_540x1200.RAW", 1200, 500);
-            var image2 = new Int16Image("sino801_540x1200.RAW", 1200, 500);
-            image1.LoadUint16File2d("sino800_540x1200.RAW", 1200, 500);
-            image2.LoadUint16File2d("sino801_540x1200.RAW", 1200, 500);
-            var image3 = image1.CompareImages2d(image2);
-            image3.Output2dToFile();
+            var image1= new Int16Image("sino800_540x1200.RAW", 1200, 540);
+            var image2 = new Int16Image("sino801_540x1200.RAW", 1200, 540);
+            image1.LoadUint16File("sino800_540x1200.RAW", 1200, 540);
+            image2.LoadUint16File("sino801_540x1200.RAW", 1200, 540);
+            var image3 = image1.CompareImagesAsync(image2).GetAwaiter().GetResult();
+            image3.OutputToFile();
         }
-
-
     }
 }
